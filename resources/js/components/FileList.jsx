88435f41fddd0b5c7017b83fd053831ba8174b55
Mauricio <mauricio@casadaweb.net>
2024-05-24 17:53:03 -0300

    Front End Finalizado

--- resources/js/components/FileList.jsx
+++ resources/js/components/FileList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function FileList({ updateFiles }) {
     const [files, setFiles] = useState([]);
@@ -14,29 +14,30 @@ function FileList({ updateFiles }) {
                 throw new Error('Erro ao obter a lista de arquivos.');
             }
             const data = await response.json();
-            setFiles(data.files.map(file => {
-                // Formatando o nome do arquivo removendo o caminho completo
-                return file.split('/').pop();
-            }));
+            setFiles(data.files.map(file => file.split('/').pop())); // Formatando o nome do arquivo removendo o caminho completo
         } catch (error) {
             console.error('Erro ao obter a lista de arquivos:', error);
         }
     };
 
-    return (
-        <div>
-            <h2>Lista de Arquivos CSV</h2>
-            <ul>
-                {files.length > 0 ? (
-                    files.map((file, index) => (
-                        <li key={index}>{file}</li>
-                    ))
-                ) : (
-                    <li>Nenhum arquivo encontrado.</li>
-                )}
-            </ul>
-        </div>
-    );
+    const fileListContent = useMemo(() => {
+        return (
+            <div>
+                <h2>Lista de Arquivos CSV</h2>
+                <ul>
+                    {files.length > 0 ? (
+                        files.map((file, index) => (
+                            <li key={index}>{file}</li>
+                        ))
+                    ) : (
+                        <li>Nenhum arquivo encontrado.</li>
+                    )}
+                </ul>
+            </div>
+        );
+    }, [files]);
+
+    return fileListContent;
 }
 
 export default FileList;
