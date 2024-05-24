a1098eb2b8ccb68159323774590d2774f17c2647
Mauricio <mauricio@casadaweb.net>
2024-05-24 17:57:45 -0300

    .

---
+++ storage/framework/views/7b81e9d66e659d009c8159887d285870.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload CSV</title>
+    
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+</head>
+<body>
+    <div id="csv-upload-form"></div>
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\mauricio_kan_teste\resources\views/upload.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/ca29df9bd2e8771cb3893b2c5561fcfd.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Upload do CSV</title>
+
+    <!-- Links para os arquivos CSS do Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    <!-- Adicione o link para o seu script JS compilado -->
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/js/app.jsx']); ?>
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\mauricio_kan_teste\resources\views/welcome.blade.php ENDPATH**/ ?>
