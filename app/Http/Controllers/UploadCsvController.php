d2e79dac9f5e61baac28b6ddda8e6a0c2ddd5052
Mauricio <mauricio@casadaweb.net>
2024-05-24 19:34:32 -0300

    .

--- app/Http/Controllers/UploadCsvController.php
+++ app/Http/Controllers/UploadCsvController.php
@@ -12,18 +12,14 @@ class UploadCsvController extends Controller
         if ($request->hasFile('file')) {
             $file = $request->file('file');
 
-            // Verifique se é um arquivo CSV
             if ($file->getClientOriginalExtension() !== 'csv') {
                 return response()->json(['error' => 'O arquivo enviado não é um arquivo CSV.'], 400);
             }
 
-            // Salve o arquivo em storage/csv (crie o diretório se não existir)
             $path = $file->storeAs('csv', $file->getClientOriginalName());
 
-            // Despache um trabalho para processar o arquivo CSV
-            ProcessCsvFile::dispatch($path)->onQueue('high'); // Coloque o trabalho em uma fila de alta prioridade
+            ProcessCsvFile::dispatch($path)->onQueue('high');
 
-            // Retorna uma resposta indicando que o arquivo foi recebido para processamento
             return response()->json(['message' => 'Arquivo CSV enviado para processamento.'], 200);
         }
 
