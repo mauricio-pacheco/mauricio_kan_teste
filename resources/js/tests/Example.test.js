2345c8fd7487e0d9c1cd665cb22f1d52a8345cc0
Mauricio <mauricio@casadaweb.net>
2024-05-24 20:10:40 -0300

    .

--- resources/js/tests/Example.test.js
+++ resources/js/tests/Example.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // Para adicionar os matchers do Jest-Dom
-import Example from '../../resources/js/components/Example'; // Importe o componente a ser testado
+import '@testing-library/jest-dom/extend-expect';
+import Example from '../../resources/js/components/Example';
 
 describe('Example component', () => {
   test('renders file input and submit button', () => {
-    render(<Example />); // Renderize o componente
-
-    // Verifique se os elementos do formulário estão presentes na tela
+    render(<Example />);
     const fileInput = screen.getByLabelText('Carregar Arquivo');
     const submitButton = screen.getByText('Carregar Arquivo');
 
@@ -16,22 +14,18 @@ describe('Example component', () => {
   });
 
   test('handles file upload and displays upload progress', () => {
-    render(<Example />); // Renderize o componente
+    render(<Example />);
 
-    // Simule o envio de um arquivo
     const file = new File(['file contents'], 'file.csv', { type: 'text/csv' });
     const fileInput = screen.getByLabelText('Carregar Arquivo');
     fireEvent.change(fileInput, { target: { files: [file] } });
 
-    // Simule o envio do formulário
     const submitButton = screen.getByText('Carregar Arquivo');
     fireEvent.click(submitButton);
 
-    // Verifique se a mensagem de upload está sendo exibida
     const uploadMessage = screen.getByText('Arquivo enviado com sucesso.');
     expect(uploadMessage).toBeInTheDocument();
 
-    // Verifique se o progresso de upload está sendo exibido
     const uploadProgress = screen.getByText('0%');
     expect(uploadProgress).toBeInTheDocument();
   });
